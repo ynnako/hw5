ae5b49f8fd43446865c4df6864ece49d273d7265
Yonatan <ynnako@gmail.com>
2019-01-23 00:41:16 +0200

    fixed determinant

--- sq_mat_impl.h
+++ sq_mat_impl.h
@@ -8,36 +8,41 @@
 #endif //HW5_SQ_MAT_IMPL_H
 
 template <class T>
+SqMat<T>::SqMat(Mat<T> m):
+Mat<T>(m){
+    if(this->height() != this->width()) throw ExceptionWrongDimensions();
+}
+
+template <class T>
 unsigned int  SqMat<T>::size() const {
 
-	return height;
+	return this->width();
 }
 
 
 template <class T>
 T SqMat<T>::determinant() const {
 
-	if (size() == 1) return *(this->begin());
-	unsigned int size = size();
+	if (size() == 1) return *(this->begin()->begin());
+	unsigned int size = this->size();
 	Mat<T> tmp_SqMat(size);
-	SqMat<T> SqMat_calc(size-1);
+	//SqMat<T> SqMat_calc(size-1);
 	Vec<unsigned int> ind1;
 	Vec<unsigned int> ind2;
-	Vec<T> row_vec;
-
+    int i , exp;
 	T det_res = 0;
-	
-	int i;
-	auto p_first_row = this->begin();
-	row_vec = this->getrows(range(0,1));
+
 	for (i = 0 ; i < size ; i++ )
 	{
-		ind1 = (range( 0 , i - 1 );
-		ind2 = (range( i + 1 , size - i );
-		tmp_SqMat = this->get_rows( range(1 , size-1));
-		SqMat_calc = tmp_SqMat->get_cols( ind1, ind2 );
-		det_res += ((-1) ^ (i)) * row_vec[i] * SqMat_calc.determinant() ;
+		ind1 = (range((unsigned int) 0 , i));
+		ind2 = (range((unsigned int) i + 1 , size - 1 -i ));
+		tmp_SqMat = this->get_rows( range((unsigned int)1 , size-1));
+        SqMat<T> SqMat_calc (tmp_SqMat.get_cols( (ind1, ind2) ));
+        if(i % 2) exp = -1 ;
+        else exp = 1 ;
+		det_res +=  exp * (*(this->begin()))[i] * SqMat_calc.determinant() ;
 	}
+	return det_res;
 }
 
 
