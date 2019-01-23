06e8f13f7eaca1b375ef65e10bb67a1369b25ef0
orgab <isr641@gmail.com>
2019-01-23 10:54:29 +0200

    add complex funcrions

--- Complex.cpp
+++ Complex.cpp
@@ -28,23 +28,28 @@ Complex Complex::operator-(const Complex &rhs) const {
     return Complex();
 }
 
-Complex Complex::operator=(const Complex &rhs) const {
-    return Complex();
-}
+
 
 bool Complex::operator==(const Complex &rhs) const {
-    return false;
+	bool equal;
+	if (this->m_re != rhs.m_re || this->m_im != rhs.m_im) equal = 0;
+	else equal = 1;
+
+    return equal ;
 }
 
 bool Complex::operator<(const Complex &rhs) {
-    return false;
+	double abs_square_left = ( this->m_re * this->m_re ) + ( this->m_im * this->m_im );
+	double abs_square_right = (rhs.m_re * rhs.m_re) + (rhs.m_im * rhs.m_im);
+
+	return abs_square_left < abs_square_right;
 }
 
 double Complex::im() const {
-    return 0;
+    return m_im;
 }
 
 double Complex::re() const {
-    return 0;
+    return m_re;
 }
 
