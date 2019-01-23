da50cbd64d0915834264b1ed2c6c5cf0d0de997c
Yonatan <ynnako@gmail.com>
2019-01-23 10:42:15 +0200

    added implementations declerations

--- Complex.cpp
+++ Complex.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "Complex.h"
+
+Complex::Complex() {
+
+}
+
+Complex::Complex(double r) {
+
+}
+
+Complex::Complex(double r, double i) {
+
+}
+
+Complex Complex::operator+(const Complex &rhs) const {
+    return Complex();
+}
+
+Complex Complex::operator*(const Complex &rhs) const {
+    return Complex();
+}
+
+Complex Complex::operator-(const Complex &rhs) const {
+    return Complex();
+}
+
+Complex Complex::operator=(const Complex &rhs) const {
+    return Complex();
+}
+
+bool Complex::operator==(const Complex &rhs) const {
+    return false;
+}
+
+bool Complex::operator<(const Complex &rhs) {
+    return false;
+}
+
+double Complex::im() const {
+    return 0;
+}
+
+double Complex::re() const {
+    return 0;
+}
+
